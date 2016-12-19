367bee04d4bfbe967c6796f822af911ec02a58df
Administrator <Administrator@7A304-0033>
2016-12-19 20:32:17 +0800

    补充

--- src/main/java/com/swqs/schooltrade/entity/Image.java
+++ src/main/java/com/swqs/schooltrade/entity/Image.java
@@ -1,5 +1,31 @@
 package com.swqs.schooltrade.entity;
 
-public class Image {
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+
+import com.swqs.schooltrade.util.BaseEntity;
+
+@Entity
+public class Image extends BaseEntity {
+
+	Goods goodsId;
+	String pictureUrl;
+
+	@ManyToOne(optional = false)
+	public Goods getGoodsId() {
+		return goodsId;
+	}
+
+	public void setGoodsId(Goods goodsId) {
+		this.goodsId = goodsId;
+	}
+
+	public String getPictureUrl() {
+		return pictureUrl;
+	}
+
+	public void setPictureUrl(String pictureUrl) {
+		this.pictureUrl = pictureUrl;
+	}
 
 }
